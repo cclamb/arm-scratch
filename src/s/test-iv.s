7a0555b8d806d10703bbb7d6833eb598d0937d33
cclamb <cclamb@ece.unm.edu>
2013-06-05 13:35:40 -0600

    asm

--- src/s/test-iv.s
+++ src/s/test-iv.s
@@ -1,9 +1,20 @@
 .global _Reset
 _Reset:
-    b   _Test_Mov
+    b   _Test_Shift
 
 _Test_Mov:
     ldr r5, =0x05 /* r5  = 5 */
     ldr r7, =0x08 /* r7  = 8 */
-    mov r7,r5
+    mov r7, r5
     bkpt
+
+_Test_Shift:
+    ldr r5, =0x01 /* r5  = 5 */
+    ldr r7, =0x08 /* r7  = 8 */
+    mov r7, r5, LSL #2
+    bkpt
+
+_Square:
+    mul r1, r0, r0
+    mov r0, r1
+    mov pc, lr
