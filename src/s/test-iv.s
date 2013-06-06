75b4ad4489fdaa7912812b8466d3a65fc7128f56
cclamb <cclamb@cs.unm.edu>
2013-06-06 06:35:04 -0600

    more experiments with asm

--- src/s/test-iv.s
+++ src/s/test-iv.s
@@ -1,6 +1,6 @@
 .global _Reset
 _Reset:
-    b   _Test_Shift
+    b   _Subroutines
 
 _Test_Mov:
     ldr r5, =0x05 /* r5  = 5 */
@@ -14,7 +14,19 @@ _Test_Shift:
     mov r7, r5, LSL #2
     bkpt
 
-_Square:
-    mul r1, r0, r0
-    mov r0, r1
+_Test_B_Shifter:
+    ldr r5, =0x01
+    mov r7, r5, LSL #3
+    mov r8, r7, LSR #1
+    bkpt
+
+_Subroutines:
+    ldr r3, =0x01
+    bl _Sub_Shift
+    mov r8, r7, LSR #1
+    bkpt 
+
+_Sub_Shift:
+    ldr r5, =0x01
+    mov r7, r5, LSL #4
     mov pc, lr
