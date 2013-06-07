b41c818c8bac2789d740948ae74702f14f22366b
cclamb <cclamb@cs.unm.edu>
2013-06-07 05:59:51 -0600

    ld str work

--- src/s/test-iv.s
+++ src/s/test-iv.s
@@ -1,6 +1,32 @@
 .global _Reset
 _Reset:
-    b   _Subroutines
+    ldr sp, =stack_top
+    b   _LoadStore
+
+_Pop_Stack:
+    mov pc, lr
+    bkpt
+
+_Push_Stack:
+    mov pc, lr
+    bkpt
+
+_LoadStore:
+    ldr r5, =0x05
+    str r5, [sp]
+    ldr r3, [sp]
+    bkpt
+
+_Subroutines:
+    ldr r3, =0x01
+    bl  _Sub_Shift
+    mov r8, r7, LSR #1
+    bkpt 
+
+_Sub_Shift:
+    ldr r5, =0x01
+    mov r7, r5, LSL #4
+    mov pc, lr
 
 _Test_Mov:
     ldr r5, =0x05 /* r5  = 5 */
@@ -19,14 +45,3 @@ _Test_B_Shifter:
     mov r7, r5, LSL #3
     mov r8, r7, LSR #1
     bkpt
-
-_Subroutines:
-    ldr r3, =0x01
-    bl _Sub_Shift
-    mov r8, r7, LSR #1
-    bkpt 
-
-_Sub_Shift:
-    ldr r5, =0x01
-    mov r7, r5, LSL #4
-    mov pc, lr
